a67fc7e2dcfd91c33e2f50892e135f1b2c245d20
Nelson Foltz <nelsfoltz@gmail.com>
2021-02-12 15:27:06 -0500

    add: functionality for prev/next btns

--- app.js
+++ app.js
@@ -53,9 +53,32 @@ let currentItem = 0;
 
 // load initial item
 window.addEventListener('DOMContentLoaded', function () {
+  showPerson();
+});
+
+// show person based on item
+function showPerson() {
   const item = reviews[currentItem];
   img.src = item.img;
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
+}
+
+// show next person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson();
+});
+
+//show previous person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson();
 });
